5722db0cfb51e8be829a3c042582a24293d57c90
junsu1220 <crab7157@gmail.com>
2022-02-03 15:19:33 +0900

    create project

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const str = `
 010-1234-5678
 thesecon@gmail.com
-http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
 `
 
-// const regexp = new RegExp('the', 'gi')
-const regexp = /the/gi
-console.log(str.match(regexp))
+const h = `  the hello  world   !
+
+`
+
+console.log(
+  str.match(/(?<=@).{1,}/g)
+)
